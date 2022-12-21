b834668e034d33dcc07bbd14fa2470fbe46a8b13
fross2500 <60294721+fross2500@users.noreply.github.com>
2022-12-21 14:39:00 -0500

    updated contact page

--- contactus.php
+++ contactus.php
@@ -1,5 +1,5 @@
 <?php
-    $title = 'Home'; 
+    $title = 'Contact Us'; 
 
     require_once 'includes/header.php'; 
    require_once 'db/conn.php'; 
@@ -8,9 +8,49 @@
 ?>
    
    
-    <h4 class="text-center"> (contact us text) </h4>
+    <h4 class="text-center"> Contact Us</h4>
     <div class="card-body">
+
+    <br>
     
+    <table class="table">
+  <thead>
+    <tr>
+      
+      <th scope="col">Email</th>
+      <th scope="col">Social Media</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+     
+    <td>animecentral@gmail.com</td>
+
+      <td>Facebook</td>
+      <td> <a href="https://www.facebook.com/">@animecentral</a> </td>
+    </tr>
+    <tr>
+     
+      <td></td>
+      <td>Twitter</td>
+      <td><a href="https://twitter.com/">@anime_central </a></td>
+    </tr>
+   
+    <td></td>
+      <td>Instagram</td>
+      <td> <a href="https://www.instagram.com/">@animecentral1</a></td>
+    </tr>
+
+  </tbody>
+</table>
+
+<br>
+<br>
+<div class="mapouter"><div class="gmap_canvas"><iframe class="gmap_iframe" width="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" 
+src="https://maps.google.com/maps?width=800&amp;height=400&amp;hl=en&amp;q=jamaica&amp;t=h&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+<a href="https://piratebay-proxys.com/">Piratebay</a></div><style>.mapouter{position:relative;text-align:right;width:100%;height:400px;}.gmap_canvas 
+{overflow:hidden;background:none!important;width:100%;height:400px;}.gmap_iframe {height:400px!important;}</style></div>
     </div>
 
 
